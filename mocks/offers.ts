eedf1eba1c27bb054473a9b429a648ec4faf4492
Igor Kondratyev <32reggion@mail.ru>
2025-05-22 10:58:55 +0300

    Разбиваем на компоненты: прогресс

---
+++ mocks/offers.ts
@@ -0,0 +1,120 @@
+import { Offer } from '../types/offer';
+
+
+export const offers:Offer[] = [
+  {
+    "id": "4603247b-052a-4633-8f51-ca346c1d8050",
+    "title": "The house among olive ",
+    "type": "house",
+    "price": 606,
+    "previewImage": "https://15.design.htmlacademy.pro/static/hotel/18.jpg",
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 48.868610000000004,
+      "longitude": 2.342499,
+      "zoom": 16
+    },
+    "isFavorite": false,
+    "isPremium": true,
+    "rating": 2.6
+  },
+  {
+    "id": "779831d9-d964-42e9-b485-e2f38debace9",
+    "title": "Wood and stone place",
+    "type": "hotel",
+    "price": 292,
+    "previewImage": "https://15.design.htmlacademy.pro/static/hotel/1.jpg",
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 48.858610000000006,
+      "longitude": 2.330499,
+      "zoom": 16
+    },
+    "isFavorite": false,
+    "isPremium": true,
+    "rating": 2
+  },
+  {
+    "id": "c0221916-2b05-47a8-8d14-e7509a532d70",
+    "title": "Canal View Prinsengracht",
+    "type": "apartment",
+    "price": 279,
+    "previewImage": "https://15.design.htmlacademy.pro/static/hotel/18.jpg",
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 48.834610000000005,
+      "longitude": 2.335499,
+      "zoom": 16
+    },
+    "isFavorite": false,
+    "isPremium": true,
+    "rating": 2.8
+  },
+  {
+    "id": "8696f83b-5eb8-4126-9d67-da7c256ca05a",
+    "title": "Wood and stone place",
+    "type": "room",
+    "price": 188,
+    "previewImage": "https://15.design.htmlacademy.pro/static/hotel/11.jpg",
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 48.85761,
+      "longitude": 2.358499,
+      "zoom": 16
+    },
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 4
+  },
+  {
+    "id": "cd30a195-81f8-4ef4-8d5e-c1633b7b49e6",
+    "title": "Beautiful & luxurious apartment at great location",
+    "type": "apartment",
+    "price": 140,
+    "previewImage": "https://15.design.htmlacademy.pro/static/hotel/16.jpg",
+    "city": {
+      "name": "Paris",
+      "location": {
+        "latitude": 48.85661,
+        "longitude": 2.351499,
+        "zoom": 13
+      }
+    },
+    "location": {
+      "latitude": 48.87561,
+      "longitude": 2.375499,
+      "zoom": 16
+    },
+    "isFavorite": false,
+    "isPremium": false,
+    "rating": 1.9
+  },
+]
