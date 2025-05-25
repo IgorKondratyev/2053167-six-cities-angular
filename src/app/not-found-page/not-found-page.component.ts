c398594018ac17f76487ae1f3688e5122d93a293
Igor Kondratyev <32reggion@mail.ru>
2025-05-25 14:06:16 +0300

    Add  smth

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found-page',
-  imports: [],
+  standalone: true,
+  selector: 'app-not-found',
+  imports: [RouterModule],
   templateUrl: './not-found-page.component.html',
-  styleUrl: './not-found-page.component.css'
+  styleUrls: ['./not-found-page.component.css'],
 })
-export class NotFoundPageComponent {
-
-}
+export class NotFoundPageComponent {}
